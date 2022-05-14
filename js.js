941e0f389e9bb78edfa29235fd386d56f0447083
Ardzhuna <gigaru@bk.ru>
2022-05-15 02:51:12 +0300

    start

---
+++ js.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let loremText = document.querySelector('.lorem');
+    let loremList = loremText.textContent.split('');
+    let secondList = loremText.textContent.split('');
+    const play = document.querySelector('.play');
+    const audio = document.querySelector('.audio');
+    const body = document.body;
+    loremText.textContent = '';
+
+
+    play.addEventListener('click', function() {
+        if (document.getElementsByTagName('audio').length === 0) {
+            setInterval(() => {
+                let letter = loremList.shift();
+                if (letter) loremText.textContent += letter
+
+
+
+            }, 1)
+            const dropping = setInterval(() => {
+                const drop = document.createElement('div');
+                drop.classList.add('drop-letter');
+                drop.textContent = secondList.shift();
+                drop.style.left = Math.random() * 1360 + 'px'
+                body.prepend(drop);
+
+            }, 500)
+            const audio = document.createElement('audio');
+            audio.src = './frontend.mp3';
+            audio.autoplay = true;
+            body.append(audio)
+        }
+
+    })
+})
